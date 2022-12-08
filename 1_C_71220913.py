df927b8ca2de24b1064e25efd9b5afe7f57235ef
frederiksamra <117893539+frederiksamra@users.noreply.github.com>
2022-12-08 16:04:13 +0700

    Add files via upload

---
+++ 1_C_71220913.py
@@ -0,0 +1,8 @@
+text =input("Masukkan Kata atau Angka : ")
+print(text[::-1])
+
+text =input("Masukkan Kata atau Angka : ")
+print(text[::-1])
+
+text =input("Masukkan Kata atau Angka : ")
+print(text[::-1])
