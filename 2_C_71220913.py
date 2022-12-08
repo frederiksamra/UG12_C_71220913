2e16bc84ef2f42249b3f3f7f8160e72b83c614ec
frederiksamra <117893539+frederiksamra@users.noreply.github.com>
2022-12-08 16:10:13 +0700

    Add files via upload

---
+++ 2_C_71220913.py
@@ -0,0 +1,30 @@
+a = input("Masukkan angka :")
+b = input("Masukkan angka yang dihitung :")
+jumlah = 0
+for i in range(len(a)):
+    if a[i] == b :
+        jumlah += 1
+
+print("Angka {} muncul sebanyak {}".format(b,jumlah))
+
+a = input("Masukkan angka :")
+b = input("Masukkan angka yang dihitung :")
+jumlah = 0
+for i in range(len(a)):
+    if a[i] == b :
+        jumlah += 1
+
+print("Angka {} muncul sebanyak {}".format(b,jumlah))
+
+
+a = input("Masukkan angka :")
+b = input("Masukkan angka yang dihitung :")
+jumlah = 0
+for i in range(len(a)):
+    if a[i] == b :
+        jumlah += 1
+
+print("Angka {} muncul sebanyak {}".format(b,jumlah))
+
+
+
